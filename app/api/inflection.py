22e421fe00bfbe485da35f86d3cc559b184aa4a6
CGH233 <1113713599@qq.com>
2020-02-24 15:46:29 +0800

    涨幅更新

--- app/api/inflection.py
+++ app/api/inflection.py
@@ -37,8 +37,20 @@ def data_list():
             return jsonify({"information": {}}), 201
         definite_increase = "Null"
         if data.newdefinite is not None and data.definite is not None:
-            definite_increase = (float)data.newdefinite / (float)(data.definite-data.newdefinite) * 100
-            definite_increase = (str)definite_increase + "%"
+            definite_increase = float(data.newdefinite) / float(data.definite-data.newdefinite) * 100
+            definite_increase = str(definite_increase) + "%"
+        suspected_increase = "Null"
+        if data.newsuspected is not None and data.suspected is not None:
+            suspected_increase = float(data.newsuspected) / float(data.suspected - data.newsuspected) * 100
+            suspected_increase = str(definite_increase) + "%"
+        death_increase = "Null"
+        if data.newdeath is not None and data.death is not None:
+            death_increase = float(data.newdeath) / float(data.death - data.newdeath) * 100
+            death_increase = str(death_increase) + "%"
+        cured_increase = "Null"
+        if data.newcured is not None and data.cured is not None:
+            cured_increase = float(data.newcured) / float(data.cured - data.newcured) * 100
+            cured_increase = str(cured_increase) + "%"
         information = {"date": data.date,
                        "total": data.total,
                        "definite": data.definite,
@@ -48,7 +60,10 @@ def data_list():
                        "newdefinite": data.newdefinite,
                        "definite_increase": definite_increase,
                        "newdeath": data.newdeath,
+                       "death_increase": death_increase,
                        "newsuspected": data.newsuspected,
+                       "suspected_increase": suspected_increase,
                        "newcured": data.newcured,
+                       "cured_increase": cured_increase
                        }
         return jsonify({"information": information}), 200
